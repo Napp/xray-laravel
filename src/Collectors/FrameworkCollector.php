08c4d5449f1d954a5a241294c3764a939ac8ea20
Mads Møller <mm@napp.dk>
2020-03-16 14:31:15 +0100

    LARAVEL_START is not present in Vapor

--- src/Collectors/FrameworkCollector.php
+++ src/Collectors/FrameworkCollector.php
@@ -9,9 +9,8 @@ class FrameworkCollector extends EventsCollector
     public function registerEventListeners(): void
     {
         // Application and Laravel startup times
-        // LARAVEL_START is defined at the entry point of the application
-        // https://github.com/laravel/laravel/blob/master/public/index.php#L10
-        $this->addSegment('composer autoload', LARAVEL_START);
+        $startTime = defined('LARAVEL_START') ? LARAVEL_START : microtime(true);
+        $this->addSegment('composer autoload', $startTime);
 
         $this->app->booting(function () {
             $this->addSegment('laravel boot');
