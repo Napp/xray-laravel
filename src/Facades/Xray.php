89f71dbadebc2029468c1f9aaffe5f35df75b1de
Mads Møller <mm@napp.dk>
2020-03-17 16:27:28 +0100

    facade

--- src/Facades/Xray.php
+++ src/Facades/Xray.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace Napp\Xray\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Pkerrigan\Xray\Segment;
+use Pkerrigan\Xray\Trace;
+use Symfony\Component\HttpFoundation\Request;
 
 class Xray extends Facade
 {
     /**
-     * @inheritDoc
+     * @method static Trace tracer()
+     * @method static Segment current()
+     * @method static bool isEnabled()
+     * @method static Segment addSegment(string $name, ?float $startTime = null, ?array $metadata = null)
+     * @method static Segment addCustomSegment(Segment $segment, string $name)
+     * @method static null|Segment getSegment(string $name)
+     * @method static void endSegment(string $name)
+     * @method static bool hasAddedSegment(string $name)
+     * @method static void endCurrentSegment()
+     * @method static void initHttpTracer(Request $request)
+     * @method static void initCliTracer(string $name)
+     * @method static void submitHttpTracer($response)
+     * @method static void submitCliTracer()
+     *
+     * @see \Napp\Xray\Xray
      */
     protected static function getFacadeAccessor()
     {
--- src/Xray.php
+++ src/Xray.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Napp\Xray;
 
 use Napp\Xray\Collectors\SegmentCollector;
+use Pkerrigan\Xray\Segment;
+use Pkerrigan\Xray\Trace;
 use Symfony\Component\HttpFoundation\Request;
 
 class Xray
@@ -16,9 +18,49 @@ class Xray
         $this->collector = $collector;
     }
 
+    public function tracer(): Trace
+    {
+        return $this->collector->tracer();
+    }
+
+    public function current(): Segment
+    {
+        return $this->collector->current();
+    }
+
     public function isEnabled(): bool
     {
         return $this->collector->isTracerEnabled();
+    }
+
+    public function addSegment(string $name, ?float $startTime = null, ?array $metadata = null): Segment
+    {
+        return $this->collector->addSegment($name, $startTime, $metadata);
+    }
+
+    public function addCustomSegment(Segment $segment, string $name): Segment
+    {
+        return $this->collector->addCustomSegment($segment, $name);
+    }
+
+    public function getSegment(string $name): ?Segment
+    {
+        return $this->collector->getSegment($name);
+    }
+
+    public function endSegment(string $name): void
+    {
+        $this->collector->endSegment($name);
+    }
+
+    public function hasAddedSegment(string $name): bool
+    {
+        return $this->collector->hasAddedSegment($name);
+    }
+
+    public function endCurrentSegment(): void
+    {
+        $this->collector->endCurrentSegment();
     }
 
     public function initHttpTracer(Request $request): void
