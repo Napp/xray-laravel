fd0a26a27fb878cb5372713c182502b3af1c622d
Mads Møller <mm@napp.dk>
2020-03-17 17:52:30 +0100

    improve the view collector

--- src/Collectors/ViewCollector.php
+++ src/Collectors/ViewCollector.php
@@ -9,12 +9,14 @@ class ViewCollector extends EventsCollector
     public function registerEventListeners(): void
     {
         $this->app['events']->listen('creating:*', function ($view, $data = [])  {
-            $this->addSegment('View ' . $view)->end();
+            $viewName = substr($view, 10);
+            $this->addSegment('View ' . $viewName)->end();
         });
 
         $this->app['events']->listen('composing:*', function ($view, $data = [])  {
-            if ($this->hasAddedSegment('View ' . $view)) {
-                $this->endSegment('View ' . $view);
+            $viewName = substr($view, 11);
+            if ($this->hasAddedSegment('View ' . $viewName)) {
+                $this->endSegment('View ' . $viewName);
             }
         });
     }
