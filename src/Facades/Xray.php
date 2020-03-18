a586899353bc0ea06e9e045b22f27e91eec1c26c
Mads Møller <mm@napp.dk>
2020-03-18 14:34:30 +0100

    move the facade doc

--- src/Facades/Xray.php
+++ src/Facades/Xray.php
@@ -9,24 +9,27 @@ use Napp\Xray\Segments\Trace;
 use Pkerrigan\Xray\Segment;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @method static Trace tracer()
+ * @method static Segment current()
+ * @method static bool isEnabled()
+ * @method static Segment addSegment(string $name, ?float $startTime = null, ?array $metadata = null)
+ * @method static Segment addCustomSegment(Segment $segment, string $name)
+ * @method static null|Segment getSegment(string $name)
+ * @method static void endSegment(string $name)
+ * @method static bool hasAddedSegment(string $name)
+ * @method static void endCurrentSegment()
+ * @method static void initHttpTracer(Request $request)
+ * @method static void initCliTracer(string $name)
+ * @method static void submitHttpTracer($response)
+ * @method static void submitCliTracer()
+ *
+ * @see \Napp\Xray\Xray
+ */
 class Xray extends Facade
 {
     /**
-     * @method static Trace tracer()
-     * @method static Segment current()
-     * @method static bool isEnabled()
-     * @method static Segment addSegment(string $name, ?float $startTime = null, ?array $metadata = null)
-     * @method static Segment addCustomSegment(Segment $segment, string $name)
-     * @method static null|Segment getSegment(string $name)
-     * @method static void endSegment(string $name)
-     * @method static bool hasAddedSegment(string $name)
-     * @method static void endCurrentSegment()
-     * @method static void initHttpTracer(Request $request)
-     * @method static void initCliTracer(string $name)
-     * @method static void submitHttpTracer($response)
-     * @method static void submitCliTracer()
-     *
-     * @see \Napp\Xray\Xray
+     * {@inheritDoc}
      */
     protected static function getFacadeAccessor()
     {
