8f95464b756bdfae6ffde6bf96f002b0b9f79d33
Shueh Chou Lu <evanlu361425@gmail.com>
2021-06-29 11:36:13 +0800

    CI test laravel 5 (#1)
    * test: create mock object by builder
    
    * ci: uncomment 7.3-5.8
    
    * ci: allow PR for CI
    
    * fix: try comment test to make it pass
    
    * ci: fix php unit version
    
    * ci: allow 7.2-5.x 7.3-5.6
    
    * uncomment test

--- tests/Collectors/SegmentCollectorTest.php
+++ tests/Collectors/SegmentCollectorTest.php
@@ -7,15 +7,13 @@ namespace Napp\Xray\Tests;
 use Illuminate\Http\Request;
 use Napp\Xray\Collectors\SegmentCollector;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 
 class SegmentCollectorTest extends TestCase
 {
     public function test_disable_trace_if_filtered()
     {
-        $request = $this->createMock(Request::class);
-        $request->expects($this->any())->method('ip')->willReturn('some-ip');
-        $request->expects($this->any())->method('url')->willReturn('some-url');
-        $request->expects($this->any())->method('method')->willReturn('GET');
+        $request = $this->createRequest();
         $request->expects($this->any())->method('path')->willReturn('filter/path');
 
         $segment = new SegmentCollector();
@@ -26,16 +24,26 @@ class SegmentCollectorTest extends TestCase
 
     public function test_should_enable_trace()
     {
-        $request = $this->createMock(Request::class);
-        $request->expects($this->any())->method('ip')->willReturn('some-ip');
-        $request->expects($this->any())->method('url')->willReturn('some-url');
-        $request->expects($this->any())->method('method')->willReturn('GET');
+        $request = $this->createRequest();
         $request->expects($this->any())->method('path')->willReturn('normal/path');
 
         $segment = new SegmentCollector();
         $segment->initHttpTracer($request);
 
         $this->assertTrue($segment->tracer()->isSampled());
+    }
+
+    protected function createRequest(): MockObject
+    {
+        $request = $this->getMockBuilder(Request::class)
+            ->setMethods(['ip', 'url', 'method', 'path'])
+            ->getMock();
+
+        $request->expects($this->any())->method('ip')->willReturn('some-ip');
+        $request->expects($this->any())->method('url')->willReturn('some-url');
+        $request->expects($this->any())->method('method')->willReturn('GET');
+
+        return $request;
     }
 
     /**
