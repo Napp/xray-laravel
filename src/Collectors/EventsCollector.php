9393d336ad1a2d213dc91dbc24a6570d58114d15
SachinBahukhandi <sachinb0013@gmail.com>
2024-04-11 12:32:39 +0530

    Added the  as a property

--- src/Collectors/EventsCollector.php
+++ src/Collectors/EventsCollector.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Application;
 abstract class EventsCollector extends SegmentCollector
 {
 
+    protected $app;
     public function __construct()
     {
         $this->app = app();
